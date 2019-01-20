aa8a29ff0e944e1c071bd44a2e8bebd023cb07d6
Rapheal Schotola <e1225193@student.tuwien.ac.at>
2019-01-20 13:25:00 +0100

    added file to test list slicing

---
+++ src/listTest.py
@@ -0,0 +1,7 @@
+# how to get x (or x-1) elements of a list from the back and in backwards order
+x = 10
+
+l = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+print(l[-x:])
+print(l[:-x:-1])
